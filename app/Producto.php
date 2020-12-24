c0b78a378d4f2ce1bc5906debb071023edba1834
Alexander Briones <alexanderbvart@gmail.com>
2020-12-24 13:55:20 -0500

    aliminar el descuento de stock

--- app/Producto.php
+++ app/Producto.php
@@ -31,8 +31,5 @@ class Producto extends Model
         return $this->hasMany('App\DetalleVenta', 'producto_id', 'producto_id');
     }  
     
-    public static function ActualizarStock($producto_id,$cantidad){
-        return DB::select("UPDATE productos set cantidad = cantidad - '".$cantidad."' where producto_id='".$producto_id."'"
-        );
-    }
+
 }
